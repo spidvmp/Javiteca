b3db24be394e4fdc563da6e81d6150eb5022ea1f
Vicente de Miguel <spidvmp@gmail.com>
2015-11-02 12:39:58 +0100

    mejorado albumes

--- public_html/scripts/services/AlbumsProvider.js
+++ public_html/scripts/services/AlbumsProvider.js
@@ -1,8 +1,12 @@
 angular.module("javi").service("AlbumsProvider",["$http",
     function($http)
     {
-        this.getAlbums = function()
-                     {
-                       return  $http.get("data/albums.json");
-                     };
+        this.getAlbums = function() {
+            return  $http.get("data/albums.json");
+        };
+
+        this.getTracks = function() {
+
+
+        }
     }]);
