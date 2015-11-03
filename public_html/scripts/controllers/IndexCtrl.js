96afc81875be98985cb35798ff0a0fbd85814c5d
Vicente de Miguel <spidvmp@gmail.com>
2015-11-03 13:40:26 +0100

    Puesto control de las solapas en menu principal y de las imagenes si no existen

--- public_html/scripts/controllers/IndexCtrl.js
+++ public_html/scripts/controllers/IndexCtrl.js
@@ -1,6 +1,16 @@
-angular.module("javi").controller("IndexCtrl",["$routeSegment","$scope",function($routeSegment,$scope){
+angular.module("javi").controller("IndexCtrl",["$route","$scope",function($route,$scope){
 
+//pongo controles para saber que opcion de labarra han pulsado y mover la clase 'active' para que se mueva la sola activa
+    $scope.rutaAlbum=function(){
+        return $route.current && $route.current.$$route.originalPath === '/album';
+    }
 
+    $scope.rutaBanda=function() {
+        return $route.current && $route.current.$$route.originalPath === '/banda';
+    }
 
+    $scope.rutaGenero=function() {
+        return $route.current && $route.current.$$route.originalPath === '/genero';
+    }
 
 }]);
