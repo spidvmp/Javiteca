3d4464fdd7c5b0dd7ccdca0d2dfd8ee78e5b43f1
Vicente de Miguel <spidvmp@gmail.com>
2015-11-06 19:18:43 +0100

    Funciona en banda y generos

--- public_html/scripts/directives/bandaDirect.js
+++ public_html/scripts/directives/bandaDirect.js
@@ -4,6 +4,14 @@ angular.module("javi").directive("bandaDirect", ["Javiteca", function(){
         templateUrl: "views/bandaDirect.html",
         scope: {
             ba: "="
+        },
+        link: function (scope) {
+
+            //pulsan en el tr del album
+            scope.seleccionado = function(){
+                console.log("seleccionado");
+                alert("seleccionan " + scope.ba.name);
+            }
         }
     };
 }]);
--- public_html/scripts/directives/generoDirect.js
+++ public_html/scripts/directives/generoDirect.js
@@ -4,6 +4,14 @@ angular.module("javi").directive("generoDirect", ["Javiteca", function(){
         templateUrl: "views/generoDirect.html",
         scope: {
             ge: "="
+        },
+        link: function (scope) {
+
+            //pulsan en el tr del album
+            scope.seleccionado = function(){
+                console.log("seleccionado");
+                alert("seleccionan " + scope.ge.name);
+            }
         }
     };
 }]);
