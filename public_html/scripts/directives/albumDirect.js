dfa165157aa46b1134b397c0bc1f20ae2c2d3997
Vicente de Miguel <spidvmp@gmail.com>
2015-11-06 19:15:03 +0100

    No funciona con el tr, pero si con el td. Puesto en albumes

--- public_html/scripts/directives/albumDirect.js
+++ public_html/scripts/directives/albumDirect.js
@@ -4,6 +4,14 @@ angular.module("javi").directive("albumDirect", ["Javiteca", function(){
         templateUrl: "views/albumDirect.html",
         scope: {
             al: "="
+        },
+        link: function (scope) {
+
+            //pulsan en el tr del album
+            scope.seleccionado = function(){
+                console.log("seleccionado");
+                alert("seleccionan " + scope.al.title);
+            }
         }
     };
 }]);
